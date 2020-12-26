a923e4d4d099ccd403a0e58578a2547193f07b70
Mawathilde <mawathilde@gmail.com>
2020-12-26 19:15:06 +0100

    Change mod update url

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,8 +537,8 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-        //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
+        //const distroURL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+        const distroURL = 'http://37.59.175.81/modpack/distribution.json'
         const opts = {
             url: distroURL,
             timeout: 2500
