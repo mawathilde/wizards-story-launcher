f1ae99a6c94336bb7ff1d1a8397a72d9a3dcb9de
jordanglemaitre <jordanglemaitre@gmail.com>
2021-01-14 02:17:25 +0100

    change state value

--- app/assets/js/discordwrapper.js
+++ app/assets/js/discordwrapper.js
@@ -11,7 +11,7 @@ exports.initRPC = function(genSettings, servSettings, initialDetails = 'En atten
 
     activity = {
         details: initialDetails,
-        state: 'Serveur: ' + servSettings.shortId,
+        state: 'discord.gg/jQsUH5f',
         largeImageKey: servSettings.largeImageKey,
         largeImageText: servSettings.largeImageText,
         smallImageKey: genSettings.smallImageKey,
